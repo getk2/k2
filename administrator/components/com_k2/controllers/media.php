0131f3214eb0fccc14986370b2a7ce409bd0bb0e
Lefteris Kavadas <lefteris.kavadas@gmail.com>
2013-10-31 13:55:38 +0200

    Fixed issue regarding media manager.

--- administrator/components/com_k2/controllers/media.php
+++ administrator/components/com_k2/controllers/media.php
@@ -8,75 +8,111 @@
  */
 
 // no direct access
-defined('_JEXEC') or die;
+defined('_JEXEC') or die ;
 
 jimport('joomla.application.component.controller');
+jimport('joomla.filesystem.file');
 
 class K2ControllerMedia extends K2Controller
 {
 
-    public function display($cachable = false, $urlparams = array())
-    {
-        JRequest::setVar('view', 'media');
-        parent::display();
-    }
+	public function display($cachable = false, $urlparams = array())
+	{
+		JRequest::setVar('view', 'media');
+		parent::display();
+	}
 
-    function connector()
-    {
-        $mainframe = JFactory::getApplication();
-        $params = JComponentHelper::getParams('com_media');
-        $root = $params->get('file_path', 'media');
-        $folder = JRequest::getVar('folder', $root, 'default', 'path');
-        $type = JRequest::getCmd('type', 'video');
-        if (JString::trim($folder) == "")
-        {
-            $folder = $root;
-        }
-        $url = JURI::root(true).'/'.$folder;
-        $path = JPATH_SITE.DS.JPath::clean($folder);
-        JPath::check($path);
-        include_once JPATH_COMPONENT_ADMINISTRATOR.DS.'lib'.DS.'elfinder'.DS.'elFinderConnector.class.php';
-        include_once JPATH_COMPONENT_ADMINISTRATOR.DS.'lib'.DS.'elfinder'.DS.'elFinder.class.php';
-        include_once JPATH_COMPONENT_ADMINISTRATOR.DS.'lib'.DS.'elfinder'.DS.'elFinderVolumeDriver.class.php';
-        include_once JPATH_COMPONENT_ADMINISTRATOR.DS.'lib'.DS.'elfinder'.DS.'elFinderVolumeLocalFileSystem.class.php';
-        function access($attr, $path, $data, $volume)
-        {
-            $mainframe = JFactory::getApplication();
-            // Hide files and folders starting with .
-            if (strpos(basename($path), '.') === 0 && $attr == 'hidden')
-            {
-                return true;
-            }
-            // Read only access for front-end. Full access for administration section.
-            switch($attr)
-            {
-                case 'read' :
-                    return true;
-                    break;
-                case 'write' :
-                    return ($mainframe->isSite()) ? false : true;
-                    break;
-                case 'locked' :
-                    return ($mainframe->isSite()) ? true : false;
-                    break;
-                case 'hidden' :
-                    return false;
-                    break;
-            }
+	function connector()
+	{
+		$mainframe = JFactory::getApplication();
+		$params = JComponentHelper::getParams('com_media');
+		$root = $params->get('file_path', 'media');
+		$folder = JRequest::getVar('folder', $root, 'default', 'path');
+		$type = JRequest::getCmd('type', 'video');
+		if (JString::trim($folder) == "")
+		{
+			$folder = $root;
+		}
+		else
+		{
+			// Ensure that we are always below the root directory
+			if (strpos($folder, $root) !== 0)
+			{
+				$folder = $root;
+			}
+		}
 
-        }
+		// Disable debug
+		JRequest::setVar('debug', false);
 
-        if ($mainframe->isAdmin())
-        {
-            $permissions = array('read' => true, 'write' => true);
-        }
-        else
-        {
-            $permissions = array('read' => true, 'write' => false);
-        }
-        $options = array('roots' => array( array('driver' => 'LocalFileSystem', 'path' => $path, 'URL' => $url, 'accessControl' => 'access', 'defaults' => $permissions)));
-        $connector = new elFinderConnector(new elFinder($options));
-        $connector->run();
-    }
+		$url = JURI::root(true).'/'.$folder;
+		$path = JPATH_SITE.DS.JPath::clean($folder);
+
+		JPath::check($path);
+		include_once JPATH_COMPONENT_ADMINISTRATOR.DS.'lib'.DS.'elfinder'.DS.'elFinderConnector.class.php';
+		include_once JPATH_COMPONENT_ADMINISTRATOR.DS.'lib'.DS.'elfinder'.DS.'elFinder.class.php';
+		include_once JPATH_COMPONENT_ADMINISTRATOR.DS.'lib'.DS.'elfinder'.DS.'elFinderVolumeDriver.class.php';
+		include_once JPATH_COMPONENT_ADMINISTRATOR.DS.'lib'.DS.'elfinder'.DS.'elFinderVolumeLocalFileSystem.class.php';
+		function access($attr, $path, $data, $volume)
+		{
+			$mainframe = JFactory::getApplication();
+			// Hide PHP files.
+			$ext = strtolower(JFile::getExt(basename($path)));
+			if ($ext == 'php')
+			{
+				return true;
+			}
+
+			// Hide files and folders starting with .
+			if (strpos(basename($path), '.') === 0 && $attr == 'hidden')
+			{
+				return true;
+			}
+			// Read only access for front-end. Full access for administration section.
+			switch($attr)
+			{
+				case 'read' :
+					return true;
+					break;
+				case 'write' :
+					return ($mainframe->isSite()) ? false : true;
+					break;
+				case 'locked' :
+					return ($mainframe->isSite()) ? true : false;
+					break;
+				case 'hidden' :
+					return false;
+					break;
+			}
+
+		}
+
+		if ($mainframe->isAdmin())
+		{
+			$permissions = array(
+				'read' => true,
+				'write' => true
+			);
+		}
+		else
+		{
+			$permissions = array(
+				'read' => true,
+				'write' => false
+			);
+		}
+		$options = array(
+			'debug' => false,
+			'roots' => array( array(
+					'driver' => 'LocalFileSystem',
+					'path' => $path,
+					'URL' => $url,
+					'accessControl' => 'access',
+					'defaults' => $permissions
+				))
+		);
+		$connector = new elFinderConnector(new elFinder($options));
+		$connector->run();
+	}
 
 }
