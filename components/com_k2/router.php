60cfa5f1c2cdf41b38bdb1e317bb5504764444b4
Lefteris Kavadas (Office) <lefteriskavadasoffice@Lefteriss-iMac.local>
2014-04-03 13:45:17 +0300

    Fixed canonical URL issue when advanced SEF is enabled.

--- components/com_k2/router.php
+++ components/com_k2/router.php
@@ -407,7 +407,7 @@ if ($params->get('k2Sef'))
 
 				default :
 					$vars['id'] = $segments[1];
-					if(isset($segments[2]) && strpos($segments[2], ':'))
+					if(isset($segments[2]))
 					{
 						$vars['id'] .= ':'.str_replace(':', '-', $segments[2]);
 					}
