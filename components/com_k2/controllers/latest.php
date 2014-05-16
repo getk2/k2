135dae0e5cfb572c72d95dfd17818605dbf7da0c
Lefteris Kavadas (Office) <lefteriskavadasoffice@Lefteriss-iMac.local>
2014-05-16 13:34:26 +0300

    Fixed issue in latest view when caching is enabled. Fixed #155.

--- components/com_k2/controllers/latest.php
+++ components/com_k2/controllers/latest.php
@@ -7,29 +7,33 @@
  * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
  */
 // no direct access
-defined('_JEXEC') or die;
+defined('_JEXEC') or die ;
 
 jimport('joomla.application.component.controller');
 
 class K2ControllerLatest extends K2Controller
 {
-    public function display($cachable = false, $urlparams = array())
-    {
-        $view = $this->getView('latest', 'html');
-        $model = $this->getModel('itemlist');
-        $view->setModel($model);
-        $itemModel = $this->getModel('item');
-        $view->setModel($itemModel);
-        $user = JFactory::getUser();
-        if ($user->guest)
-        {
-            $cache = true;
-        }
-        else
-        {
-            $cache = false;
-        }
-        parent::display($cache);
-    }
+	public function display($cachable = false, $urlparams = array())
+	{
+		$view = $this->getView('latest', 'html');
+		$model = $this->getModel('itemlist');
+		$view->setModel($model);
+		$itemModel = $this->getModel('item');
+		$view->setModel($itemModel);
+		$user = JFactory::getUser();
+		if ($user->guest)
+		{
+			$cache = true;
+		}
+		else
+		{
+			$cache = false;
+		}
+		if (K2_JVERSION != '15')
+		{
+			$urlparams['Itemid'] = 'INT';
+		}
+		parent::display($cache, $urlparams);
+	}
 
 }
