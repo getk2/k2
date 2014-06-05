a52a38309d0ca7c8a7537cd0a56c16dc47de18bc
Christelle <christelle-olivier@laposte.net>
2014-06-05 13:59:35 +0200

    Prevent double encoding
    I understand the problem of the PHP version so to prevent double encoding, I propose to test the PHP version first.
    
    If the version is 5.2.3 or higher, the best way is to set the new parameter "double_encode" to false and if the version is lower in this case a simple solution is to decode the text first.
    
    Christelle

--- components/com_k2/helpers/utilities.php
+++ components/com_k2/helpers/utilities.php
@@ -165,7 +165,10 @@ class K2HelperUtilities
 	// Cleanup HTML entities
 	public static function cleanHtml($text)
 	{
-		return htmlentities($text, ENT_QUOTES, 'UTF-8', false);
+		if (version_compare(PHP_VERSION, '5.2.3', '<'))
+			return htmlentities(html_entity_decode($text, ENT_QUOTES, 'UTF-8'), ENT_QUOTES, 'UTF-8'); // To prevent double encoding
+		else
+			return htmlentities($text, ENT_QUOTES, 'UTF-8', false);
 	}
 
 	// Gender
