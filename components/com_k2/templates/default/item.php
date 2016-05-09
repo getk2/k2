94ffb46d47bb30ec966b14315815653ed9372a0e
Diego Luis Soler <solerdiego@gmail.com>
2016-05-09 13:34:45 -0300

    Multi-language Facebook Button
    By default K2 always shows the English version of the Facebook button.
    This fix  provides a multi-language Facebook button.

--- components/com_k2/templates/default/item.php
+++ components/com_k2/templates/default/item.php
@@ -318,7 +318,11 @@ defined('_JEXEC') or die;
 		<!-- Facebook Button -->
 		<div class="itemFacebookButton">
 			<div id="fb-root"></div>
-			<script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)) return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));</script>
+                        <?php 
+                        $lang = JFactory::getApplication()->getLanguage();
+                        $langTag = str_replace('-', '_', $lang->getTag());
+                        ?>
+                        <script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)) return;js=d.createElement(s);js.id=id;js.src="<?php echo "//connect.facebook.net/{$langTag}/sdk.js#xfbml=1&version=v2.5"; ?>";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));</script>
 			<div class="fb-like" data-width="200" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
 		</div>
 		<?php endif; ?>
