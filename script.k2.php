179dee6f319e67ccf288edd75809e87429f35a9a
Fotis Evangelou <fevangelou@users.noreply.github.com>
2017-02-18 06:06:37 +0200

    Minor update to the installer script

--- script.k2.php
+++ script.k2.php
@@ -41,12 +41,12 @@ class Com_K2InstallerScript
                 }
                 JFile::move(JPATH_SITE.'/plugins/'.$group.'/'.$name.'/'.$name.'.j25.xml', JPATH_SITE.'/plugins/'.$group.'/'.$name.'/'.$name.'.xml');
             }
-			if($group != 'finder')
-			{
-		    	$query = "UPDATE #__extensions SET enabled=1 WHERE type='plugin' AND element=".$db->Quote($name)." AND folder=".$db->Quote($group);
-            	$db->setQuery($query);
-            	$db->query();
-			}
+            if($group != 'finder')
+            {
+                $query = "UPDATE #__extensions SET enabled=1 WHERE type='plugin' AND element=".$db->Quote($name)." AND folder=".$db->Quote($group);
+                $db->setQuery($query);
+                $db->query();
+            }
             $status->plugins[] = array('name' => $name, 'group' => $group, 'result' => $result);
         }
         $modules = $manifest->xpath('modules/module');
@@ -60,11 +60,11 @@ class Com_K2InstallerScript
             }
             ($client == 'administrator') ? $path = $src.'/administrator/modules/'.$name : $path = $src.'/modules/'.$name;
 
-			if($client == 'administrator')
-			{
-				$db->setQuery("SELECT id FROM #__modules WHERE `module` = ".$db->quote($name));
-				$isUpdate = (int)$db->loadResult();
-			}
+            if($client == 'administrator')
+            {
+                $db->setQuery("SELECT id FROM #__modules WHERE `module` = ".$db->quote($name));
+                $isUpdate = (int)$db->loadResult();
+            }
 
             $installer = new JInstaller;
             $result = $installer->install($path);
@@ -78,18 +78,18 @@ class Com_K2InstallerScript
                 JFile::move($root.'/modules/'.$name.'/'.$name.'.j25.xml', $root.'/modules/'.$name.'/'.$name.'.xml');
             }
             $status->modules[] = array('name' => $name, 'client' => $client, 'result' => $result);
-			if($client == 'administrator' && !$isUpdate)
-			{
-				$position = version_compare(JVERSION, '3.0', '<') && $name == 'mod_k2_quickicons'? 'icon' : 'cpanel';
-				$db->setQuery("UPDATE #__modules SET `position`=".$db->quote($position).",`published`='1' WHERE `module`=".$db->quote($name));
-				$db->query();
+            if($client == 'administrator' && !$isUpdate)
+            {
+                $position = version_compare(JVERSION, '3.0', '<') && $name == 'mod_k2_quickicons'? 'icon' : 'cpanel';
+                $db->setQuery("UPDATE #__modules SET `position`=".$db->quote($position).",`published`='1' WHERE `module`=".$db->quote($name));
+                $db->query();
 
-				$db->setQuery("SELECT id FROM #__modules WHERE `module` = ".$db->quote($name));
-				$id = (int)$db->loadResult();
+                $db->setQuery("SELECT id FROM #__modules WHERE `module` = ".$db->quote($name));
+                $id = (int)$db->loadResult();
 
-				$db->setQuery("INSERT IGNORE INTO #__modules_menu (`moduleid`,`menuid`) VALUES (".$id.", 0)");
-				$db->query();
-			}
+                $db->setQuery("INSERT IGNORE INTO #__modules_menu (`moduleid`,`menuid`) VALUES (".$id.", 0)");
+                $db->query();
+            }
         }
 
         if (JFile::exists(JPATH_ADMINISTRATOR.'/components/com_k2/admin.k2.php'))
@@ -101,19 +101,42 @@ class Com_K2InstallerScript
         {
             JFile::delete(JPATH_ADMINISTRATOR.'/components/com_k2/models/cpanel.php');
         }
-		if (version_compare(JVERSION, '3.0', 'lt') && JFolder::exists(JPATH_ADMINISTRATOR.'/components/com_joomfish/contentelements'))
-		{
-			$elements = $manifest->xpath('joomfish/file');
-			foreach ($elements as $element)
-			{
-				JFile::copy($src.'/administrator/components/com_joomfish/contentelements/'.$element->data(), JPATH_ADMINISTRATOR.'/components/com_joomfish/contentelements/'.$element->data());
-			}
-		}
+        if (version_compare(JVERSION, '3.0', 'lt') && JFolder::exists(JPATH_ADMINISTRATOR.'/components/com_joomfish/contentelements'))
+        {
+            $elements = $manifest->xpath('joomfish/file');
+            foreach ($elements as $element)
+            {
+                JFile::copy($src.'/administrator/components/com_joomfish/contentelements/'.$element->data(), JPATH_ADMINISTRATOR.'/components/com_joomfish/contentelements/'.$element->data());
+            }
+        }
 
-	    // Clean up empty entries in #__k2_users table caused by an issue in the K2 user plugin. Fix details: http://code.google.com/p/getk2/source/detail?r=1966
-		$query = "DELETE FROM #__k2_users WHERE userID = 0";
-		$db->setQuery($query);
-		$db->query();
+        // Clean up empty entries in #__k2_users table caused by an issue in the K2 user plugin. Fix details: http://code.google.com/p/getk2/source/detail?r=1966
+        $query = "DELETE FROM #__k2_users WHERE userID = 0";
+        $db->setQuery($query);
+        $db->query();
+
+        /*
+        // Fix media manager folder permissions
+        set_time_limit(0);
+        jimport('joomla.filesystem.folder');
+        jimport('joomla.filesystem.path');
+        $params = JComponentHelper::getParams('com_media');
+        $root = $params->get('file_path', 'media');
+        $mediaPath = JPATH_SITE.'/'.JPath::clean($root);
+        $folders = JFolder::folders($mediaPath, '.', true, true, array());
+        foreach($folders as $folder)
+        {
+            @chmod($folder, 0755);
+        }
+        if(JFolder::exists($mediaPath.'/'.'.tmb'))
+        {
+            @chmod($mediaPath.'/'.'.tmb', 0755);
+        }
+        if(JFolder::exists($mediaPath.'/'.'.quarantine'))
+        {
+            @chmod($mediaPath.'/'.'.quarantine', 0755);
+        }
+        */
 
         $this->installationResults($status);
 
@@ -222,7 +245,8 @@ class Com_K2InstallerScript
             $db->query();
         }
 
-        /*$query = "SHOW INDEX FROM #__k2_items";
+        /*
+        $query = "SHOW INDEX FROM #__k2_items";
         $db->setQuery($query);
         $indexes = $db->loadObjectList();
         $indexExists = false;
@@ -258,7 +282,8 @@ class Com_K2InstallerScript
             $query = "ALTER TABLE #__k2_tags ADD FULLTEXT (`name`)";
             $db->setQuery($query);
             $db->query();
-        }*/
+        }
+        */
 
         // Add index for comments count
         $query = "SHOW INDEX FROM #__k2_comments";
@@ -277,8 +302,6 @@ class Com_K2InstallerScript
             $db->setQuery($query);
             $db->query();
         }
-
-
 
         $query = "SELECT COUNT(*) FROM #__k2_user_groups";
         $db->setQuery($query);
@@ -300,23 +323,20 @@ class Com_K2InstallerScript
         if (!array_key_exists('ip', $fields))
         {
             $query = "ALTER TABLE `#__k2_users`
-			ADD `ip` VARCHAR( 15 ) NOT NULL ,
-			ADD `hostname` VARCHAR( 255 ) NOT NULL ,
-			ADD `notes` TEXT NOT NULL";
+            ADD `ip` VARCHAR( 15 ) NOT NULL ,
+            ADD `hostname` VARCHAR( 255 ) NOT NULL ,
+            ADD `notes` TEXT NOT NULL";
             $db->setQuery($query);
             $db->query();
         }
 
-
-		 $query = "CREATE TABLE IF NOT EXISTS `#__k2_log` (
-  `status` int(11) NOT NULL,
-  `response` text NOT NULL,
-  `timestamp` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-            $db->setQuery($query);
-            $db->query();
-
-
+        $query = "CREATE TABLE IF NOT EXISTS `#__k2_log` (
+        `status` int(11) NOT NULL,
+        `response` text NOT NULL,
+        `timestamp` datetime NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+        $db->setQuery($query);
+        $db->query();
 
     }
     private function installationResults($status)
@@ -375,6 +395,7 @@ class Com_K2InstallerScript
         </table>
     <?php
     }
+    
     private function uninstallationResults($status)
     {
     $language = JFactory::getLanguage();
@@ -432,4 +453,5 @@ class Com_K2InstallerScript
         </table>
     <?php
     }
-    }
+
+}
