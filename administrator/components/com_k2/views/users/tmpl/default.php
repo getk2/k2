149d14303485cd07674975ef7e052b4e0310b8f0
Fotis Evangelou <fevangelou@users.noreply.github.com>
2017-05-12 04:15:02 +0300

    Normalize hidden label for search (backend)

--- administrator/components/com_k2/views/users/tmpl/default.php
+++ administrator/components/com_k2/views/users/tmpl/default.php
@@ -28,7 +28,7 @@ $document->addScriptDeclaration("
 	<table class="k2AdminTableFilters table">
 		<tr>
 			<td class="k2AdminTableFiltersSearch">
-				<!--<label><?php echo JText::_('K2_FILTER'); ?></label>-->
+				<label class="visually-hidden"><?php echo JText::_('K2_FILTER'); ?></label>
 				<div class="btn-wrapper input-append">
 					<input type="text" name="search" value="<?php echo htmlspecialchars($this->lists['search'], ENT_QUOTES, 'UTF-8'); ?>" class="text_area"	title="<?php echo JText::_('K2_FILTER_BY_TITLE'); ?>" placeholder="<?php echo JText::_('K2_FILTER'); ?>" />
 					<button id="k2SubmitButton" class="btn"><?php echo JText::_('K2_GO'); ?></button>
