c8a30641412dc4e68802d5a95e4038ff484f966d
Fotis Evangelou <fevangelou@users.noreply.github.com>
2017-08-07 18:33:48 +0300

    Fix syntax error for previous commit

--- administrator/components/com_k2/views/tags/tmpl/default.php
+++ administrator/components/com_k2/views/tags/tmpl/default.php
@@ -80,7 +80,7 @@ $context = JRequest::getCmd('context');
 					if(JRequest::getCmd('output') == 'list'){
 						$onClick = 'window.parent.k2ModalSelector(\''.urlencode($row->name).'\', \''.str_replace(array("'", "\""), array("\\'", ""), $row->name).'\', \''.JRequest::getCmd('fid').'\', \''.JRequest::getVar('fname').'\', \''.JRequest::getCmd('output').'\'); return false;';
 					} else {
-						$onClick = 'window.parent.k2ModalSelector(\''.urlencode($row->name).'\', \''.str_replace(array("'", "\""), array("\\'", ""), $row->name).'\', \''.JRequest::getCmd('fid').'\', \''.JRequest::getVar('fname').'\'), $row->name).'\'); return false;';
+						$onClick = 'window.parent.k2ModalSelector(\''.urlencode($row->name).'\', \''.str_replace(array("'", "\""), array("\\'", ""), $row->name).'\', \''.JRequest::getCmd('fid').'\', \''.JRequest::getVar('fname').'\'); return false;';
 					}
 					?>
 					<a class="k2ListItemDisabled" title="<?php echo JText::_('K2_CLICK_TO_ADD_THIS_ENTRY'); ?>" href="#" onclick="<?php echo $onClick; ?>"><?php echo $row->name; ?></a>
