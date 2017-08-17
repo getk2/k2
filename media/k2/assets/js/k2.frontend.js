97dfc3fd6ec2a1178c548da63d7e678b2b22d7ed
Fotis Evangelou <fevangelou@users.noreply.github.com>
2017-08-17 22:32:56 +0300

    Code formatting

--- media/k2/assets/js/k2.frontend.js
+++ media/k2/assets/js/k2.frontend.js
@@ -13,27 +13,25 @@ $K2(document).ready(function(){
 	// Magnific popup
 	if(typeof($K2.magnificPopup) !== 'undefined') {
 		// --- Backwards compatibility for users with overrides - START ---
-
 		// First remove possible attached events of the core modal in case it is loaded by a third party extension
 		$K2('.itemImage a.modal, .itemEditLink a.modal, .catItemEditLink a.modal, .catItemAddLink a.modal, .userItemAddLink a.modal, .userItemEditLink a.modal, .k2UserBlockActions a.modal, .k2UserBlockRenderedMenu a.modal, .commentToolbar a.modal').unbind('click');
 
 		// Item image
-		$K2('.itemImage a.modal').magnificPopup({type:'image', image: {titleSrc : function() {return '';}}});
+		$K2('.itemImage a.modal').magnificPopup({type:'image', image: {titleSrc: function() {return '';}}});
 
 		// Edit links
 		$K2('.itemEditLink a.modal, .catItemEditLink a.modal, .catItemAddLink a.modal, .userItemAddLink a.modal, .userItemEditLink a.modal').magnificPopup({type:'iframe', modal: true});
 
 		// Any other link
 		$K2('.k2UserBlockActions a.modal, .k2UserBlockRenderedMenu a.modal, .commentToolbar a.modal').magnificPopup({type:'iframe'});
-
 		// --- Backwards compatibility for users with overrides - FINISH ---
 
 		// New layouts
-		// Image
-		$K2('[data-k2-modal="image"]').magnificPopup({type:'image', image: {titleSrc : function() {return '';}}});
+		// Images
+		$K2('[data-k2-modal="image"]').magnificPopup({type:'image', image: {titleSrc: function() {return '';}}});
 		// Edit links
 		$K2('[data-k2-modal="edit"]').magnificPopup({type:'iframe', modal: true});
-		// Rest links
+		// Generic iframe
 		$K2('[data-k2-modal="iframe"]').magnificPopup({type:'iframe'});
 	}
 
