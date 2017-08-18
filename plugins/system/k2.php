e58e52fd385a072b59f8a3045656eb2d9550e56d
Fotis Evangelou <fevangelou@users.noreply.github.com>
2017-08-18 21:15:52 +0300

    Change version to 2.8.1 [Dev Build]

--- plugins/system/k2.php
+++ plugins/system/k2.php
@@ -32,7 +32,7 @@ class plgSystemK2 extends JPlugin
 		}
 
 		// Define K2 version & build here
-		define('K2_CURRENT_VERSION', '2.9.0');
+		define('K2_CURRENT_VERSION', '2.8.1');
 		define('K2_BUILD', ' [Dev Build]'); // Use '' for stable or ' [Dev Build YYYYMMDD]' for the developer build
 
 		// Define the DS constant (for backwards compatibility with old template overrides & 3rd party K2 extensions)
