e6b875efa2cfbe58d2d88a813291ec7982725f44
Fotis Evangelou <fevangelou@users.noreply.github.com>
2017-08-18 03:31:38 +0300

    Update release date

--- plugins/system/k2.php
+++ plugins/system/k2.php
@@ -33,7 +33,7 @@ class plgSystemK2 extends JPlugin
 
 		// Define K2 version & build here
 		define('K2_CURRENT_VERSION', '2.8.0');
-		define('K2_BUILD', ''); // Use '' for stable or ' [Dev Build]' for the developer build
+		define('K2_BUILD', ''); // Use '' for stable or ' [Dev Build YYYYMMDD]' for the developer build
 
 		// Define the DS constant (for backwards compatibility with old template overrides & 3rd party K2 extensions)
 		if (!defined('DS'))
