dee168698523e526e1e6ab11c7c34d1e20c72ea3
Fotis Evangelou <fevangelou@users.noreply.github.com>
2017-10-02 17:44:25 +0300

    Proper token check in the Media controller for all J releases

--- administrator/components/com_k2/controllers/media.php
+++ administrator/components/com_k2/controllers/media.php
@@ -15,118 +15,103 @@ jimport('joomla.filesystem.file');
 
 class K2ControllerMedia extends K2Controller
 {
+    public function display($cachable = false, $urlparams = array())
+    {
+        JRequest::setVar('view', 'media');
+        parent::display();
+    }
 
-	public function display($cachable = false, $urlparams = array())
-	{
-		JRequest::setVar('view', 'media');
-		parent::display();
-	}
+    public function connector()
+    {
+	    // Check token
+        $method = ($_POST) ? 'post' : 'get';
+        if (version_compare(JVERSION, '2.5', 'ge')) {
+            JSession::checkToken($method) or jexit(JText::_('JINVALID_TOKEN'));
+        } else {
+            JRequest::checkToken($method) or jexit(JText::_('JINVALID_TOKEN'));
+        }
 
-	function connector()
-	{
+        $application = JFactory::getApplication();
+        $params = JComponentHelper::getParams('com_media');
+        $root = $params->get('file_path', 'media');
+        $folder = JRequest::getVar('folder', $root, 'default', 'path');
+        $type = JRequest::getCmd('type', 'video');
 
-		if ($_POST)
-		{
-			JSession::checkToken('post') or jexit(JText::_('JINVALID_TOKEN'));
-		}
-		else
-		{
-			JSession::checkToken('get') or jexit(JText::_('JINVALID_TOKEN'));
-		}
+        if (JString::trim($folder) == "") {
+            $folder = $root;
+        } else {
+            // Ensure that we are always below the root directory
+            if (strpos($folder, $root) !== 0) {
+                $folder = $root;
+            }
+        }
 
-		$application = JFactory::getApplication();
-		$params = JComponentHelper::getParams('com_media');
-		$root = $params->get('file_path', 'media');
-		$folder = JRequest::getVar('folder', $root, 'default', 'path');
-		$type = JRequest::getCmd('type', 'video');
+        // Disable debug
+        JRequest::setVar('debug', false);
 
-		if (JString::trim($folder) == "")
-		{
-			$folder = $root;
-		}
-		else
-		{
-			// Ensure that we are always below the root directory
-			if (strpos($folder, $root) !== 0)
-			{
-				$folder = $root;
-			}
-		}
+        $url = JURI::root(true).'/'.$folder;
+        $path = JPATH_SITE.'/'.JPath::clean($folder);
 
-		// Disable debug
-		JRequest::setVar('debug', false);
+        JPath::check($path);
 
-		$url = JURI::root(true).'/'.$folder;
-		$path = JPATH_SITE.'/'.JPath::clean($folder);
+        require_once(JPATH_SITE.'/media/k2/assets/vendors/studio-42/elfinder/php/autoload.php');
 
-		JPath::check($path);
+        function access($attr, $path, $data, $volume)
+        {
+            $application = JFactory::getApplication();
 
-		require_once(JPATH_SITE.'/media/k2/assets/vendors/studio-42/elfinder/php/autoload.php');
+            // Hide PHP files
+            $ext = strtolower(JFile::getExt(basename($path)));
 
-		function access($attr, $path, $data, $volume)
-		{
-			$application = JFactory::getApplication();
+            if ($ext == 'php') {
+                return true;
+            }
 
-			// Hide PHP files
-			$ext = strtolower(JFile::getExt(basename($path)));
+            // Hide files and folders starting with .
+            if (strpos(basename($path), '.') === 0 && $attr == 'hidden') {
+                return true;
+            }
 
-			if ($ext == 'php')
-			{
-				return true;
-			}
+            // Read only access for front-end. Full access for administration section.
+            switch ($attr) {
+                case 'read':
+                    return true;
+                    break;
+                case 'write':
+                    return ($application->isSite()) ? false : true;
+                    break;
+                case 'locked':
+                    return ($application->isSite()) ? true : false;
+                    break;
+                case 'hidden':
+                    return false;
+                    break;
+            }
+        }
 
-			// Hide files and folders starting with .
-			if (strpos(basename($path), '.') === 0 && $attr == 'hidden')
-			{
-				return true;
-			}
+        if ($application->isAdmin()) {
+            $permissions = array('read' => true, 'write' => true);
+        } else {
+            $permissions = array('read' => true, 'write' => false);
+        }
 
-			// Read only access for front-end. Full access for administration section.
-			switch($attr)
-			{
-				case 'read' :
-					return true;
-					break;
-				case 'write' :
-					return ($application->isSite()) ? false : true;
-					break;
-				case 'locked' :
-					return ($application->isSite()) ? true : false;
-					break;
-				case 'hidden' :
-					return false;
-					break;
-			}
-
-		}
-
-		if ($application->isAdmin())
-		{
-			$permissions = array('read' => true, 'write' => true);
-		}
-		else
-		{
-			$permissions = array('read' => true, 'write' => false);
-		}
-
-		$options = array(
-			'debug' => false,
-			'roots' => array(
-				array(
-					'driver' => 'LocalFileSystem',
-					'path' => $path,
-					'URL' => $url,
-					'accessControl' => 'access',
-					'defaults' => $permissions,
-					'mimeDetect' => 'internal',
-					'uploadDeny' => array('all'),
-					'uploadAllow' => array('image', 'video', 'audio', 'text/plain', 'text/html', 'application/json', 'application/pdf', 'application/zip', 'application/x-7z-compressed', 'application/x-bzip', 'application/x-bzip2', 'text/css', 'application/msword', 'application/vnd.ms-excel', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation'),
-					'uploadOrder' => array('deny', 'allow')
-				)
-			)
-		);
-		$connector = new elFinderConnector(new elFinder($options));
-		$connector->run();
-	}
-
+        $options = array(
+            'debug' => false,
+            'roots' => array(
+                array(
+                    'driver' => 'LocalFileSystem',
+                    'path' => $path,
+                    'URL' => $url,
+                    'accessControl' => 'access',
+                    'defaults' => $permissions,
+                    'mimeDetect' => 'internal',
+                    'uploadDeny' => array('all'),
+                    'uploadAllow' => array('image', 'video', 'audio', 'text/plain', 'text/html', 'application/json', 'application/pdf', 'application/zip', 'application/x-7z-compressed', 'application/x-bzip', 'application/x-bzip2', 'text/css', 'application/msword', 'application/vnd.ms-excel', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation'),
+                    'uploadOrder' => array('deny', 'allow')
+                )
+            )
+        );
+        $connector = new elFinderConnector(new elFinder($options));
+        $connector->run();
+    }
 }
