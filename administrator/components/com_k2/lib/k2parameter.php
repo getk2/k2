a430c0ad3cac7e41d367021d7744f3287fb56576
Fotis Evangelou <fevangelou@users.noreply.github.com>
2018-02-09 18:49:40 +0200

    Update version to 2.8.1 and date to Feb 9th, 2018

--- administrator/components/com_k2/lib/k2parameter.php
+++ administrator/components/com_k2/lib/k2parameter.php
@@ -3,7 +3,7 @@
  * @version     2.8.x
  * @package     K2
  * @author      JoomlaWorks http://www.joomlaworks.net
- * @copyright   Copyright (c) 2006 - 2017 JoomlaWorks Ltd. All rights reserved.
+ * @copyright   Copyright (c) 2006 - 2018 JoomlaWorks Ltd. All rights reserved.
  * @license     GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
  */
 
