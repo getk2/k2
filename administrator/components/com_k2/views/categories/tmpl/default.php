2f2fe666fdd3ec8e624fb86b59ee3a37d982ed9b
Fotis Evangelou <fevangelou@users.noreply.github.com>
2018-08-05 07:10:11 +0300

    Fix single category selection for new k2ModalSelector element

--- administrator/components/com_k2/views/categories/tmpl/default.php
+++ administrator/components/com_k2/views/categories/tmpl/default.php
@@ -150,9 +150,9 @@ $context = JRequest::getCmd('context');
 							<?php if($context == "modalselector"): ?>
 							<?php
 							if(JRequest::getCmd('output') == 'list'){
-								$onClick = 'window.parent.k2ModalSelector(\''.$row->id.'\', \''.str_replace(array("'", "\""), array("\\'", ""), $row->name).'\', \''.JRequest::getCmd('fid').'\', \''.JRequest::getVar('fname').'\', \''.JRequest::getCmd('output').'\'); return false;';
+								$onClick = 'window.parent.k2ModalSelector(\''.$row->id.'\', \''.str_replace(array("'", "\""), array("\\'", ""), $row->treename).'\', \''.JRequest::getCmd('fid').'\', \''.JRequest::getVar('fname').'\', \''.JRequest::getCmd('output').'\'); return false;';
 							} else {
-								$onClick = 'window.parent.k2ModalSelector(\''.$row->id.'\', \''.str_replace(array("'", "\""), array("\\'", ""), $row->name).'\'); return false;';
+								$onClick = 'window.parent.k2ModalSelector(\''.$row->id.'\', \''.str_replace(array("'", "\""), array("\\'", ""), $row->treename).'\', \''.JRequest::getCmd('fid').'\', \''.JRequest::getVar('fname').'\'); return false;';
 							}
 							?>
 							<a class="k2ListItemDisabled" title="<?php echo JText::_('K2_CLICK_TO_ADD_THIS_ENTRY'); ?>" href="#" onclick="<?php echo $onClick; ?>">
