4838e1058867ae29f4500bed1052a43dc548725f
Fotis Evangelou <fevangelou@users.noreply.github.com>
2019-05-07 20:35:03 +0300

    Edit form styling

--- administrator/components/com_k2/views/extrafieldsgroup/tmpl/default.php
+++ administrator/components/com_k2/views/extrafieldsgroup/tmpl/default.php
@@ -12,44 +12,44 @@ defined('_JEXEC') or die;
 
 $document = JFactory::getDocument();
 $document->addScriptDeclaration("
-	Joomla.submitbutton = function(pressbutton){
-		if (pressbutton == 'cancel') {
-			submitform( pressbutton );
-			return;
-		}
-		if (\$K2.trim(\$K2('#name').val()) == '') {
-			alert( '".JText::_('K2_GROUP_NAME_CANNOT_BE_EMPTY', true)."' );
-		} else {
-			submitform( pressbutton );
-		}
-	};
+    Joomla.submitbutton = function(pressbutton){
+        if (pressbutton == 'cancel') {
+            submitform( pressbutton );
+            return;
+        }
+        if (\$K2.trim(\$K2('#name').val()) == '') {
+            alert( '".JText::_('K2_GROUP_NAME_CANNOT_BE_EMPTY', true)."' );
+        } else {
+            submitform( pressbutton );
+        }
+    };
 ");
 
 ?>
 
 <form action="index.php" enctype="multipart/form-data" method="post" name="adminForm" id="adminForm">
-	<div class="xmlParamsFields k2GenericForm">
-		<h3>
-			<?php if($this->row->id): ?>
-				<?php echo JText::_('K2_EDIT_GROUP'); ?>
-			<?php else: ?>
-				<?php echo JText::_('K2_ADD_NEW_GROUP'); ?>
-			<?php endif; ?>
+    <div class="xmlParamsFields k2GenericForm">
+        <h3>
+            <?php if($this->row->id): ?>
+            <?php echo JText::_('K2_EDIT_GROUP'); ?>
+            <?php else: ?>
+            <?php echo JText::_('K2_ADD_NEW_GROUP'); ?>
+            <?php endif; ?>
         </h3>
         <ul class="adminformlist">
             <li>
                 <div class="paramLabel">
-                	<?php echo JText::_('K2_GROUP_NAME'); ?>
+                    <?php echo JText::_('K2_GROUP_NAME'); ?>
                 </div>
-				<div class="paramValue">
-					<input class="text_area k2TitleBox" type="text" name="name" id="name" value="<?php echo $this->row->name; ?>" size="50" maxlength="250" />
-				</div>
-			</li>
-		</ul>
-	</div>
-	<input type="hidden" name="option" value="com_k2" />
-	<input type="hidden" name="view" value="extrafieldsgroup" />
-	<input type="hidden" name="task" value="<?php echo JRequest::getVar('task'); ?>" />
-	<input type="hidden" name="id" value="<?php echo $this->row->id; ?>" />
-	<?php echo JHTML::_('form.token'); ?>
+                <div class="paramValue">
+                    <input class="text_area k2TitleBox" type="text" name="name" id="name" value="<?php echo $this->row->name; ?>" size="50" maxlength="250" />
+                </div>
+            </li>
+        </ul>
+    </div>
+    <input type="hidden" name="option" value="com_k2" />
+    <input type="hidden" name="view" value="extrafieldsgroup" />
+    <input type="hidden" name="task" value="<?php echo JRequest::getVar('task'); ?>" />
+    <input type="hidden" name="id" value="<?php echo $this->row->id; ?>" />
+    <?php echo JHTML::_('form.token'); ?>
 </form>
