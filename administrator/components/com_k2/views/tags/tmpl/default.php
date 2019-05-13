35956bde8bed76b7a9d7f8df9a2031472bb0f481
Fotis Evangelou <fevangelou@users.noreply.github.com>
2019-05-13 15:31:19 +0300

    Backend - Add "no content" placeholder message for the tags list

--- administrator/components/com_k2/views/tags/tmpl/default.php
+++ administrator/components/com_k2/views/tags/tmpl/default.php
@@ -48,7 +48,7 @@ $context = JRequest::getCmd('context');
         </table>
         <div class="table-responsive-wrap">
             <div class="table-responsive">
-                <table class="adminlist table table-striped" id="k2TagsList">
+                <table class="adminlist table table-striped<?php if(isset($this->rows) && count($this->rows) == 0): ?> nocontent<?php endif; ?>" id="k2TagsList">
                     <thead>
                         <tr>
                             <th class="center hidden-phone">#</th>
@@ -72,6 +72,7 @@ $context = JRequest::getCmd('context');
                         </tr>
                     </tfoot>
                     <tbody>
+                        <?php if(isset($this->rows) && count($this->rows) > 0): ?>
                         <?php foreach ($this->rows as $key => $row): ?>
                         <tr class="row<?php echo ($key%2); ?>">
                             <td class="k2Center center hidden-phone"><?php echo $key+1; ?></td>
@@ -95,6 +96,15 @@ $context = JRequest::getCmd('context');
                             <td class="k2Center center hidden-phone"><?php echo $row->id; ?></td>
                         </tr>
                         <?php endforeach; ?>
+                        <?php else: ?>
+                        <tr>
+                            <td colspan="6" class="k2ui-nocontent">
+                                <div class="k2ui-nocontent-message">
+                                    <i class="fa fa-list" aria-hidden="true"></i><?php echo JText::_('K2_BE_NO_TAGS_FOUND'); ?>
+                                </div>
+                            </td>
+                        </tr>
+                        <?php endif; ?>
                     </tbody>
                 </table>
             </div>
