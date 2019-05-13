0ff235adfc8f6032979093232b094771ea082a9e
Fotis Evangelou <fevangelou@users.noreply.github.com>
2019-05-13 15:29:21 +0300

    Backend - Add "no content" placeholder message for the extra field groups list

--- administrator/components/com_k2/views/extrafieldsgroups/tmpl/default.php
+++ administrator/components/com_k2/views/extrafieldsgroups/tmpl/default.php
@@ -28,7 +28,7 @@ $document->addScriptDeclaration("
 <form action="index.php" method="post" name="adminForm" id="adminForm">
     <div class="table-responsive-wrap">
         <div class="table-responsive">
-            <table class="adminlist table table-striped" id="k2ExtraFieldGroupsList">
+            <table class="adminlist table table-striped<?php if(isset($this->rows) && count($this->rows) == 0): ?> nocontent<?php endif; ?>" id="k2ExtraFieldGroupsList">
                 <thead>
                     <tr>
                         <th class="center hidden-phone">#</th>
@@ -50,6 +50,7 @@ $document->addScriptDeclaration("
                     </tr>
                 </tfoot>
                 <tbody>
+                    <?php if(isset($this->rows) && count($this->rows) > 0): ?>
                     <?php foreach ($this->rows as $key=>$row): ?>
                     <tr class="row<?php echo ($key%2); ?>">
                         <td class="k2Center center hidden-phone"><?php echo $key+1; ?></td>
@@ -58,6 +59,15 @@ $document->addScriptDeclaration("
                         <td class="hidden-phone"><?php echo $row->categories; ?></td>
                     </tr>
                     <?php endforeach; ?>
+                    <?php else: ?>
+                    <tr>
+                        <td colspan="4" class="k2ui-nocontent">
+                            <div class="k2ui-nocontent-message">
+                                <i class="fa fa-list" aria-hidden="true"></i><?php echo JText::_('K2_BE_NO_EXTRA_FIELD_GROUPS_FOUND'); ?>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php endif; ?>
                 </tbody>
             </table>
         </div>
