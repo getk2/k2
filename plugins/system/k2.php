30a2072e2e10221817cc353df5910c0141a73115
Fotis Evangelou <fevangelou@users.noreply.github.com>
2019-11-21 01:29:09 +0200

    Update build ID

--- plugins/system/k2.php
+++ plugins/system/k2.php
@@ -27,7 +27,7 @@ class plgSystemK2 extends JPlugin
 
         // Define K2 version & build here
         define('K2_CURRENT_VERSION', '2.10.1');
-        define('K2_BUILD_ID', '20191120');
+        define('K2_BUILD_ID', '20191121');
         define('K2_BUILD', '<br />[Dev Build '.K2_BUILD_ID.']'); // Use '' for stable or "<br />[Dev Build '.K2_BUILD_ID.']" for the developer build
 
         // Define the DS constant (for backwards compatibility with old template overrides & 3rd party K2 extensions)
