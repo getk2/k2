ec03b2c088910affa1068619c6ed53ad2069f621
Fotis Evangelou <fevangelou@users.noreply.github.com>
2019-11-20 23:45:35 +0200

    Revert change to K2 menu router (was introduced in K2 v2.9.1 dev)

--- components/com_k2/helpers/route.php
+++ components/com_k2/helpers/route.php
@@ -162,11 +162,14 @@ class K2HelperRoute
         } elseif (K2_JVERSION == '25') {
             $items = $menus->getItems('component_id', $component->id);
         } else {
+            /*
             // Grab menu items from all languages (/libraries/src/Menu/SiteMenu.php)
             $items = $menus->getItems(
                 array('component_id', 'language'),
                 array($component->id, null)
             );
+            */
+            $items = $menus->getItems('component_id', $component->id);
         }
         $match = null;
         foreach ($needles as $needle => $id) {
