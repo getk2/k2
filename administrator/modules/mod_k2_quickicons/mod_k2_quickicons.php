7878aab06e0cfa08aa176c38c1c90b0116a24d54
Fotis Evangelou <fevangelou@users.noreply.github.com>
2019-11-26 11:45:58 +0200

    Removed backend "online image editor" quickicon

--- administrator/modules/mod_k2_quickicons/mod_k2_quickicons.php
+++ administrator/modules/mod_k2_quickicons/mod_k2_quickicons.php
@@ -12,22 +12,17 @@ defined('_JEXEC') or die;
 
 $user = JFactory::getUser();
 
-if (K2_JVERSION != '15')
-{
-	if (!$user->authorise('core.manage', 'com_k2'))
-	{
-		return;
-	}
-	$language = JFactory::getLanguage();
-	$language->load('com_k2.dates', JPATH_ADMINISTRATOR);
-	if ($user->authorise('core.admin', 'com_k2'))
-	{
-		$user->gid = 1000;
-	}
-	else
-	{
-		$user->gid = 1;
-	}
+if (K2_JVERSION != '15') {
+    if (!$user->authorise('core.manage', 'com_k2')) {
+        return;
+    }
+    $language = JFactory::getLanguage();
+    $language->load('com_k2.dates', JPATH_ADMINISTRATOR);
+    if ($user->authorise('core.admin', 'com_k2')) {
+        $user->gid = 1000;
+    } else {
+        $user->gid = 1;
+    }
 }
 
 // JoomlaWorks reference parameters
@@ -48,26 +43,10 @@ $modLogo = (int)$params->get('modLogo', 1);
 // Component parameters
 $componentParams = JComponentHelper::getParams('com_k2');
 
-$onlineImageEditor = $componentParams->get('onlineImageEditor', 'picozu');
-
-switch($onlineImageEditor)
-{
-	default:
-	case 'picozu':
-		$onlineImageEditorName = JText::_('K2_IMG_EDITOR_PICOZU');
-		$onlineImageEditorLink = 'https://www.picozu.com/editor/';
-		break;
-	case 'gravit':
-		$onlineImageEditorName = JText::_('K2_IMG_EDITOR_GRAVIT');
-		$onlineImageEditorLink = 'https://app.designer.io/';
-		break;
-}
-
 // Load CSS & JS
 K2HelperHTML::loadHeadIncludes(true, false, true, false);
-if ($modCSSStyling)
-{
-	$document->addStyleSheet(JURI::base(true).'/modules/'.$mod_name.'/tmpl/css/style.css?v='.K2_CURRENT_VERSION);
+if ($modCSSStyling) {
+    $document->addStyleSheet(JURI::base(true).'/modules/'.$mod_name.'/tmpl/css/style.css?v='.K2_CURRENT_VERSION);
 }
 
 // Output content with template
--- administrator/modules/mod_k2_quickicons/tmpl/default.php
+++ administrator/modules/mod_k2_quickicons/tmpl/default.php
@@ -16,146 +16,138 @@ defined('_JEXEC') or die;
 
 <?php if($modLogo): ?>
 <div id="k2QuickIconsTitle">
-	<a class="dashicon k2logo" href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=items&amp;filter_featured=-1&amp;filter_trash=0'); ?>" title="<?php echo JText::_('K2_DASHBOARD'); ?>">
-		<span>K2</span>
-	</a>
+    <a class="dashicon k2logo" href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=items&amp;filter_featured=-1&amp;filter_trash=0'); ?>" title="<?php echo JText::_('K2_DASHBOARD'); ?>">
+        <span>K2</span>
+    </a>
 </div>
 <?php endif; ?>
 
-<div id="k2QuickIcons<?php if(K2_JVERSION=='15') echo '15'; ?>"<?php if(!$modLogo): ?> class="k2NoLogo"<?php endif; ?>>
-  <div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=item'); ?>">
-			<i class="dashicon item-new"></i>
-			<span><?php echo JText::_('K2_ADD_NEW_ITEM'); ?></span>
-	    </a>
+<div id="k2QuickIcons<?php if(K2_JVERSION=='15') echo '15'; ?>" <?php if(!$modLogo): ?> class="k2NoLogo" <?php endif; ?>>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=item'); ?>">
+                <i class="dashicon item-new"></i>
+                <span><?php echo JText::_('K2_ADD_NEW_ITEM'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-  <div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=items&amp;filter_featured=-1&amp;filter_trash=0'); ?>">
-			<i class="dashicon items"></i>
-			<span><?php echo JText::_('K2_ITEMS'); ?></span>
-	    </a>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=items&amp;filter_featured=-1&amp;filter_trash=0'); ?>">
+                <i class="dashicon items"></i>
+                <span><?php echo JText::_('K2_ITEMS'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-	<div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=items&amp;filter_featured=1&amp;filter_trash=0'); ?>">
-			<i class="dashicon items-featured"></i>
-			<span><?php echo JText::_('K2_FEATURED_ITEMS'); ?></span>
-	    </a>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=items&amp;filter_featured=1&amp;filter_trash=0'); ?>">
+                <i class="dashicon items-featured"></i>
+                <span><?php echo JText::_('K2_FEATURED_ITEMS'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-  <div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=items&amp;filter_featured=-1&amp;filter_trash=1'); ?>">
-			<i class="dashicon items-trashed"></i>
-			<span><?php echo JText::_('K2_TRASHED_ITEMS'); ?></span>
-	    </a>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=items&amp;filter_featured=-1&amp;filter_trash=1'); ?>">
+                <i class="dashicon items-trashed"></i>
+                <span><?php echo JText::_('K2_TRASHED_ITEMS'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-	<div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=categories&amp;filter_trash=0'); ?>">
-			<i class="dashicon categories"></i>
-			<span><?php echo JText::_('K2_CATEGORIES'); ?></span>
-	    </a>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=categories&amp;filter_trash=0'); ?>">
+                <i class="dashicon categories"></i>
+                <span><?php echo JText::_('K2_CATEGORIES'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-	<div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=categories&amp;filter_trash=1'); ?>">
-			<i class="dashicon categories-trashed"></i>
-			<span><?php echo JText::_('K2_TRASHED_CATEGORIES'); ?></span>
-	    </a>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=categories&amp;filter_trash=1'); ?>">
+                <i class="dashicon categories-trashed"></i>
+                <span><?php echo JText::_('K2_TRASHED_CATEGORIES'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-	<?php if(!$componentParams->get('lockTags') || $user->gid>23): ?>
-	<div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=tags'); ?>">
-			<i class="dashicon tags"></i>
-			<span><?php echo JText::_('K2_TAGS'); ?></span>
-	    </a>
+    <?php if(!$componentParams->get('lockTags') || $user->gid>23): ?>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=tags'); ?>">
+                <i class="dashicon tags"></i>
+                <span><?php echo JText::_('K2_TAGS'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-  <?php endif; ?>
-	<div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=comments'); ?>">
-			<i class="dashicon comments"></i>
-			<span><?php echo JText::_('K2_COMMENTS'); ?></span>
-	    </a>
+    <?php endif; ?>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=comments'); ?>">
+                <i class="dashicon comments"></i>
+                <span><?php echo JText::_('K2_COMMENTS'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-  <?php if ($user->gid>23): ?>
-  <div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=extrafields'); ?>">
-			<i class="dashicon extra-fields"></i>
-			<span><?php echo JText::_('K2_EXTRA_FIELDS'); ?></span>
-	    </a>
+    <?php if ($user->gid>23): ?>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=extrafields'); ?>">
+                <i class="dashicon extra-fields"></i>
+                <span><?php echo JText::_('K2_EXTRA_FIELDS'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-	<div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=extrafieldsgroups'); ?>">
-			<i class="dashicon extra-field-groups"></i>
-			<span><?php echo JText::_('K2_EXTRA_FIELD_GROUPS'); ?></span>
-	    </a>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=extrafieldsgroups'); ?>">
+                <i class="dashicon extra-field-groups"></i>
+                <span><?php echo JText::_('K2_EXTRA_FIELD_GROUPS'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-  <?php endif; ?>
-	<div class="icon-wrapper">
-    <div class="icon">
-	    <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=media'); ?>">
-			<i class="dashicon mediamanager"></i>
-			<span><?php echo JText::_('K2_MEDIA_MANAGER'); ?></span>
-	    </a>
+    <?php endif; ?>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a href="<?php echo JRoute::_('index.php?option=com_k2&amp;view=media'); ?>">
+                <i class="dashicon mediamanager"></i>
+                <span><?php echo JText::_('K2_MEDIA_MANAGER'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-	<div class="icon-wrapper">
-    <div class="icon">
-    	<a id="k2OnlineImageEditor" target="_blank" href="<?php echo $onlineImageEditorLink; ?>">
-			<i class="dashicon image-editing"></i>
-			<span><?php echo JText::_('K2_IMAGE_EDITOR'); ?> (<?php echo $onlineImageEditorName; ?>)</span>
-	    </a>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a data-k2-modal="iframe" target="_blank" href="https://getk2.org/documentation/">
+                <i class="dashicon documentation"></i>
+                <span><?php echo JText::_('K2_DOCS_AND_TUTORIALS'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-  <div class="icon-wrapper">
-    <div class="icon">
-    	<a data-k2-modal="iframe" target="_blank" href="https://getk2.org/documentation/">
-			<i class="dashicon documentation"></i>
-			<span><?php echo JText::_('K2_DOCS_AND_TUTORIALS'); ?></span>
-    	</a>
+    <?php if ($user->gid>23): ?>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a data-k2-modal="iframe" target="_blank" href="https://getk2.org/extend/">
+                <i class="dashicon extend"></i>
+                <span><?php echo JText::_('K2_EXTEND'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-  <?php if ($user->gid>23): ?>
-  <div class="icon-wrapper">
-    <div class="icon">
-    	<a data-k2-modal="iframe" target="_blank" href="https://getk2.org/extend/">
-			<i class="dashicon extend"></i>
-			<span><?php echo JText::_('K2_EXTEND'); ?></span>
-    	</a>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a data-k2-modal="iframe" target="_blank" href="https://www.joomlaworks.net/forum/k2">
+                <i class="dashicon help"></i>
+                <span><?php echo JText::_('K2_COMMUNITY'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-  <div class="icon-wrapper">
-    <div class="icon">
-    	<a data-k2-modal="iframe" target="_blank" href="https://www.joomlaworks.net/forum/k2">
-			<i class="dashicon help"></i>
-			<span><?php echo JText::_('K2_COMMUNITY'); ?></span>
-    	</a>
+    <div class="icon-wrapper">
+        <div class="icon">
+            <a data-k2-modal="iframe" href="https://joomlareader.com/" title="<?php echo JText::_('K2_JOOMLA_NEWS_FROM_AROUND_THE_WORLD'); ?>">
+                <i class="dashicon joomlareader"></i>
+                <span><?php echo JText::_('K2_JOOMLAREADER'); ?></span>
+            </a>
+        </div>
     </div>
-  </div>
-  <div class="icon-wrapper">
-    <div class="icon">
-	    <a data-k2-modal="iframe" href="https://joomlareader.com/" title="<?php echo JText::_('K2_JOOMLA_NEWS_FROM_AROUND_THE_WORLD'); ?>">
-			<i class="dashicon joomlareader"></i>
-			<span><?php echo JText::_('K2_JOOMLAREADER'); ?></span>
-	    </a>
-    </div>
-  </div>
-  <?php endif; ?>
-  <div style="clear: both;"></div>
+    <?php endif; ?>
+    <div style="clear: both;"></div>
 </div>
