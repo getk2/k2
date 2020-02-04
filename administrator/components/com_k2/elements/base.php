85de60758c0878d8afcd279bd7f12eea09acd7b0
Fotis Evangelou <fevangelou@users.noreply.github.com>
2020-02-04 22:10:27 +0200

    Fix notices in PHP 7.4 (base.php in K2 elements/fields)

--- administrator/components/com_k2/elements/base.php
+++ administrator/components/com_k2/elements/base.php
@@ -30,18 +30,20 @@ if (K2_JVERSION == '15') {
                 }
                 return $this->fetchElementValue($this->name, $this->value, $this->element, $this->options['control']);
                 */
-                return $this->fetchElement($this->name, $this->value, $this->element, $this->options['control']);
+                $controls = (!empty($this->options['control'])) ? $this->options['control'] : array();
+                return $this->fetchElement($this->name, $this->value, $this->element, $controls);
             }
             public function getLabel()
             {
-                if (method_exists($this, 'fetchTooltip')) { // BC
-                    return $this->fetchTooltip($this->element['label'], $this->description, $this->element, $this->options['control'], $this->element['name'] = '');
-                }
                 /*
                 if (method_exists($this, 'fetchElementName')) {
                     return $this->fetchElementName($this->element['label'], $this->description, $this->element, $this->options['control'], $this->element['name'] = '');
                 }
                 */
+                if (method_exists($this, 'fetchTooltip')) { // BC
+                    $controls = (!empty($this->options['control'])) ? $this->options['control'] : array();
+                    return $this->fetchTooltip($this->element['label'], $this->description, $this->element, $controls, $this->element['name'] = '');
+                }
                 return parent::getLabel();
             }
             public function render($layoutId, $data = array())
