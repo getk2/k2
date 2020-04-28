e54172e8165096f435eeb296ba82905d987d8759
Fotis Evangelou <fevangelou@users.noreply.github.com>
2020-04-28 13:04:50 +0300

    Ensure proper media selection in the K2 item edit form when editing content on smartphones

--- administrator/components/com_k2/views/item/tmpl/default.php
+++ administrator/components/com_k2/views/item/tmpl/default.php
@@ -267,7 +267,7 @@ $app = JFactory::getApplication();
                                 <label><?php echo JText::_('K2_ITEM_IMAGE'); ?></label>
                             </div>
                             <div class="itemAdditionalData">
-                                <input type="file" name="image" class="fileUpload k2Selector" />
+                                <input type="file" name="image" class="fileUpload k2Selector" accept="image/*" />
                                 <i>(<?php echo JText::_('K2_MAX_UPLOAD_SIZE'); ?>: <?php echo ini_get('upload_max_filesize'); ?>)</i>
 
                                 <span class="sep"><?php echo JText::_('K2_OR'); ?></span>
@@ -356,7 +356,7 @@ $app = JFactory::getApplication();
                                 <label><?php echo JText::_('K2_UPLOAD_A_ZIP_FILE_WITH_IMAGES'); ?></label>
                             </div>
                             <div class="itemAdditionalData">
-                                <input type="file" name="gallery" class="fileUpload k2Selector" />
+                                <input type="file" name="gallery" class="fileUpload k2Selector" accept=".zip" />
                                 <span class="hasTip k2GalleryNotice" title="<?php echo JText::_('K2_UPLOAD_A_ZIP_FILE_HELP_HEADER'); ?>::<?php echo JText::_('K2_UPLOAD_A_ZIP_FILE_HELP_TEXT'); ?>">
                                     <i class="fa fa-question-circle" aria-hidden="true"></i>
                                 </span>
@@ -438,7 +438,7 @@ $app = JFactory::getApplication();
                                         </ul>
                                         <div id="k2MediaTab1" class="k2TabsContent">
                                             <div class="panel" id="Upload_video">
-                                                <input type="file" name="video" class="fileUpload k2Selector" />
+                                                <input type="file" name="video" class="fileUpload k2Selector" accept="video/*,audio/*" />
                                                 <i>(<?php echo JText::_('K2_MAX_UPLOAD_SIZE'); ?>: <?php echo ini_get('upload_max_filesize'); ?>)</i>
                                             </div>
                                         </div>
