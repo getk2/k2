958eae00fde9cdd79c3b58ab6b7aac21b31e8dfd
Fotis Evangelou <fevangelou@users.noreply.github.com>
2021-06-30 03:12:08 +0300

    Code formatting for "selected tags" template

--- modules/mod_k2_tools/tmpl/selected_tags.php
+++ modules/mod_k2_tools/tmpl/selected_tags.php
@@ -15,12 +15,12 @@ defined('_JEXEC') or die;
 <?php if (count($selectedTags)): ?>
 <div id="k2ModuleBox<?php echo $module->id; ?>" class="k2SelectedTagsBlock<?php if($params->get('moduleclass_sfx')) echo ' '.$params->get('moduleclass_sfx'); ?>">
     <ul>
-    <?php foreach ($selectedTags as $key => $tag): ?>
-    <?php if ($selectedTagsLimit > 0 && ($key + 1) > $selectedTagsLimit) break; ?>
-    <li>
-        <a href="<?php echo JRoute::_(K2HelperRoute::getTagRoute(urldecode($tag))); ?>"><?php echo urldecode($tag); ?></a>
-    </li>
-    <?php endforeach; ?>
+        <?php foreach ($selectedTags as $key => $tag): ?>
+        <?php if ($selectedTagsLimit > 0 && ($key + 1) > $selectedTagsLimit) break; ?>
+        <li>
+            <a href="<?php echo JRoute::_(K2HelperRoute::getTagRoute(urldecode($tag))); ?>"><?php echo urldecode($tag); ?></a>
+        </li>
+        <?php endforeach; ?>
     </ul>
 </div>
 <?php endif; ?>
