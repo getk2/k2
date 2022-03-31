e345b7d79fd5827dcc838ae4467934d7a6dbcbac
Fotis Evangelou <fevangelou@users.noreply.github.com>
2022-03-31 19:42:19 +0300

    Update copyrights end year and date in XML files

--- components/com_k2/templates/default/latest_item.php
+++ components/com_k2/templates/default/latest_item.php
@@ -3,7 +3,7 @@
  * @version    2.11.x
  * @package    K2
  * @author     JoomlaWorks https://www.joomlaworks.net
- * @copyright  Copyright (c) 2006 - 2021 JoomlaWorks Ltd. All rights reserved.
+ * @copyright  Copyright (c) 2006 - 2022 JoomlaWorks Ltd. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
  */
 
