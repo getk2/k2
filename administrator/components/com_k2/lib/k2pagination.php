7ad7b896a4bec3aff7a54d9d7bd4f54aa26a48b3
Fotis Evangelou <fevangelou@users.noreply.github.com>
2026-06-07 19:53:04 +0300

    Restrict the use of the "ALL" option in paginated results in the K2 backend, except for admin users

--- administrator/components/com_k2/lib/k2pagination.php
+++ administrator/components/com_k2/lib/k2pagination.php
@@ -29,15 +29,22 @@ class K2Pagination extends JPagination
 
         $limits = [];
 
-        $limits[] = JHtml::_('select.option', '20', JText::_('J20'));
-        $limits[] = JHtml::_('select.option', '50', JText::_('J50'));
-        $limits[] = JHtml::_('select.option', '100', JText::_('J100'));
+        $limits[] = JHtml::_('select.option', '20', '20');
+        $limits[] = JHtml::_('select.option', '50', '50');
+        $limits[] = JHtml::_('select.option', '100', '100');
         $limits[] = JHtml::_('select.option', '200', '200');
         $limits[] = JHtml::_('select.option', '300', '300');
         $limits[] = JHtml::_('select.option', '400', '400');
         $limits[] = JHtml::_('select.option', '500', '500');
         $limits[] = JHtml::_('select.option', '1000', '1000');
-        $limits[] = JHtml::_('select.option', '0', JText::_('JALL'));
+
+        // Expose option "ALL" to site administrators only.
+        // This is to prevent browser crashes for less experienced users attempting to list thousands of items in one go.
+        $user    = JFactory::getUser();
+        $isAdmin = (K2_JVERSION == '15') ? ($user->get('gid') >= 24) : $user->authorise('core.admin');
+        if ($isAdmin) {
+            $limits[] = JHtml::_('select.option', '0', JText::_('JALL'));
+        }
 
         $selected = $this->viewall ? 0 : $this->limit;
 
